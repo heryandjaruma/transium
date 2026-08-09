d1a047ae8a340532086cfc746ecde72225204d69
RyanDJ <heryandjaruma@gmail.com>
2026-08-10 00:05:11 +0800

    Initial Commit

---
+++ transium/transiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  transiumApp.swift
+//  transium
+//
+//  Created by Heryan Djaruma on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct transiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
